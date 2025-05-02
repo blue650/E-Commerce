db976294bcd65f9899aabd5e7dabb90765f7988f
Atrone <anthony.teixeira55@gmail.com>
2025-05-02 12:17:55 -0400

    a

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,11 +3,5 @@ import dotenv from "dotenv"
 import path from "path"
 import { fileURLToPath } from "url"
 
-// Resolve project root .env file path
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-// Load environment variables from project root .env file
-dotenv.config({ path: path.resolve(__dirname, "../../.env") })
-
 export const redis = new Redis(process.env.UPSTASH_URL);
 
